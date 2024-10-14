77b0f2f01a93e5f9cb8f5d6edba7663b84094fb6
joaopedropc7 <joaopedropcunha7@gmail.com>
2024-10-14 15:03:00 -0300

    Index vercel

--- api/login.js
+++
@@ -1,9 +0,0 @@
-import { handleLoginUsuario } from '../src/routes/routes_service_MySql/routesDBMySqlServ'; // O caminho para o arquivo 1, adaptado ao seu projeto
-
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        return handleLoginUsuario(req, res);
-    } else {
-        res.status(405).json({ message: 'Método não permitido' });
-    }
-}
